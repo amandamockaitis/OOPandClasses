b7080e1a485c4052c63f35a5465a48918bb1b37d
amandamockaitis <amanda_mockaitis1@baylor.edu>
2023-02-20 11:08:54 -0600

    created food program

--- FoodClass.py
+++ FoodClass.py
@@ -1,12 +1,13 @@
 """
  Customer Class - that has the following attributes
   - customerid, name, address, email, phone, member_status (True or False). 
-  The Customer class’s __init__ method should accept an argument for each attribute. 
+  The Customer class s __init__ method should accept an argument for each attribute. 
   The Customer class should have accessor methods only for each attribute. 
 """
 
-class Customer: 
-    def __init__(self, cID, name, address, email, phone, mem_status): 
+
+class Customer:
+    def __init__(self, cID, name, address, email, phone, mem_status):
         self.__cID = cID
         self.__name = name
         self.__address = address
@@ -19,41 +20,43 @@ class Customer:
 
     def get_name(self):
         return self.__name
-    
+
     def get_address(self):
         return self.__address
-    
+
     def get_email(self):
         return self.__email
 
     def get_phone(self):
         return self.__phone
-    
+
     def get_mem_status(self):
         return self.__mem_status
+
 
 """
 Transaction Class - that has the following attributes
  - date, item name,cost and customerid. 
- The Procedure class’s __init__ method should accept an argument for each attribute. 
+ The Procedure class s __init__ method should accept an argument for each attribute. 
  The Procedure class should have accessor methods only for each attribute.
 """
 
+
 class Transaction(Customer):
-    def __init__(self, cID, name, address, email, phone, mem_status, date, item_name, cost): 
+    def __init__(
+        self, cID, name, address, email, phone, mem_status, tdate, item_name, cost
+    ):
         Customer.__init__(self, cID, name, address, email, phone, mem_status)
 
-        self.__date = date
+        self.__tdate = tdate
         self.__item_name = item_name
-        self.__cost = cost 
+        self.__cost = cost
 
-    def get_date(self):
-        return self.__date
+    def get_tdate(self):
+        return self.__tdate
 
     def get_item_name(self):
         return self.__item_name
 
     def get_cost(self):
         return self.__cost
-
-    
--- FoodProgram.py
+++ FoodProgram.py
@@ -1,17 +1,53 @@
 import FoodClass as fc
 
+
 # this dictionary represents transactions. The key of the dictionary is the transaction identifier.
 # The Value of the dictionary is a list. The elements in each list are -
 # ['Date', 'Name of item', 'Cost', 'customerid' ]
+def main():
+    dict = {
+        "trans1": ["2/15/2023", "The Lone Patty", 17, 569],
+        "trans2": ["2/15/2023", "The Octobreakfast", 18, 569],
+        "trans3": ["2/15/2023", "The Octoveg", 16, 570],
+        "trans4": ["2/15/2023", "The Octoburger", 20, 570],
+    }
 
-dict = {'trans1':['2/15/2023','The Lone Patty',17,569],
-                'trans2':['2/15/2023','The Octobreakfast',18,569],
-                'trans3':['2/15/2023','The Octoveg',16,570],
-                'trans4':['2/15/2023','The Octoburger',20,570],
-}
+    order_total = 0
 
-order_total = 0
+    # cID, name, address, email, phone, mem_status, date, item_name, cost
+    """
+    transaction1 = fc.Transaction(
+        "570",
+        "Danni Sellyar",
+        "97 Mitchell Way Hewitt Texas 76712",
+        "dsellyarft@gmpg.org",
+        "254-555-9362",
+        "False",
+    )
+    """
+    for transaction in dict:
+        cID = dict[transaction][3]
+        if cID == 570:
+            cost = dict[transaction][2]
+            # order_total += cost
+            transaction1 = fc.Transaction(
+                "570",
+                "Danni Sellyar",
+                "97 Mitchell Way Hewitt Texas 76712",
+                "dsellyarft@gmpg.org",
+                "254-555-9362",
+                "False",
+                dict[transaction][0],
+                dict[transaction][1],
+                dict[transaction][2],
+            )
+            order_total += cost
+            print("Customer Name:", transaction1.get_name())
+            print("Phone Number:", transaction1.get_phone())
+            print(
+                f"Order Item:{transaction1.get_item_name()} Price: ${format(transaction1.get_cost(), '.2f')}"
+            )
+            print(f"Total: ${format(order_total, '.2f')}")
 
 
-
-
+main()
